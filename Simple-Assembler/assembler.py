0994abbf3627327aef73cf967a05ad879225866b
pranav20395 <88434431+pranav20395@users.noreply.github.com>
2021-08-04 18:09:56 +0530

    assz

--- Simple-Assembler/assembler.py
+++ Simple-Assembler/assembler.py
@@ -5,7 +5,8 @@ import compiler
 
 def main():
     # code = sys.stdin.readlines()
-    code=open('/Users/aayushgakhar/Documents/GitHub/CO_M21_Assignment/automatedTesting/tests/assembly/errorGen/test1')
+    #code=open('/Users/aayushgakhar/Documents/GitHub/CO_M21_Assignment/automatedTesting/tests/assembly/errorGen/test1')
+    code = open('/CO/ass co/CO_M21_Assignment/automatedTesting/tests/assembly/errorGen/test1')
     print([i for i in code])
     if(error_detection.check(code)):
         print('error')
