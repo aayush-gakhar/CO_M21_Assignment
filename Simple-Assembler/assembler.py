5c6d48d92e17fc6877c98ae104d075bc84e3e976
Aayush Gakhar <aayush20006@iiitd.ac.in>
2021-08-04 21:51:51 +0530

    error_detect

--- Simple-Assembler/assembler.py
+++ Simple-Assembler/assembler.py
@@ -6,7 +6,8 @@ import compiler
 def main():
     # code = sys.stdin.readlines()
     try:
-        code=open('/Users/aayushgakhar/Documents/GitHub/CO_M21_Assignment/automatedTesting/tests/assembly/errorGen/test1')
+        code = open('/Users/aayushgakhar/Documents/GitHub/CO_M21_Assignment/automatedTesting/tests/assembly/errorGen'
+                    '/test1')
     except:
         try:
             code = open('/CO/ass co/CO_M21_Assignment/automatedTesting/tests/assembly/errorGen/test1')
@@ -14,11 +15,11 @@ def main():
             # shrishti apna path idher likh le
             pass
     print([i for i in code])
-    if(error_detection.check(code)):
+    if error_detection.check(code):
         print('error')
     compiler.compile(code)
 
 
 main()
-var=dict()
-lab=dict()
+var = dict()
+lab = dict()
--- Simple-Assembler/error_detection.py
+++ Simple-Assembler/error_detection.py
@@ -3,39 +3,41 @@ import sys
 
 def check(code):
     # return True if error in code !!!
-    return halt_check(code) or typo(code)
+    return halt_check(code) or iterate(code)
 
 
-def typo(code):
+def iterate(code):
     var_flag = True
-    for line in code:
+    for line_no, line in enumerate(code, start=1):
         line = line.split()
         if not line:
             continue
         elif line[0] == 'var' and not var_flag:
             'VarInMidError'
         elif var_flag and line[0] == 'var':
-            return variable_check(line)
+            return variable_check(line, line_no)
         elif line[0][-1] == ':':
             var_flag = False
-            return label_check(line) and instruction_check(line[1:])
+            return label_check(line, line_no) and instruction_check(line[1:], line_no)
         elif line[0] in Instructions:
             var_flag = False
-            return instruction_check(line)
+            return instruction_check(line, line_no)
         else:
-            sys.stdout.write('NameError')
+            raise_error(0, line_no)
             return False
 
 
-def variable_check(line):
-    'VariableError'
+def variable_check(line, line_no):
+    return len(line) == 2 and all(i in '_0123456789abcdefghijklmnopqrstuvwxyz' for i in line[1].lower())
 
 
-def instruction_check(line):
-    'NameError'
+def instruction_check(line, line_no):
+    if line[0] not in Instructions:
+        raise_error(0, line_no)
+        return False
 
 
-def label_check(line):
+def label_check(line, line_no):
     'LabelError'
 
 
@@ -43,12 +45,12 @@ def halt_check(code):
     try:
         f = [i for i in code].index('hlt')
         if f != len(code) - 1:
-            raise_error(8,f)
+            raise_error(8, f)
             return True
         else:
             return False
-    except:
-        raise_error(7,len(code-1))
+    except ValueError:
+        raise_error(7, len(code - 1))
         return True
 
 
