9e10d2133b3761cd7140dcf33f225ca508ae0e81
Aayush Gakhar <aayush20006@iiitd.ac.in>
2021-08-04 18:16:18 +0530

    c

--- Simple-Assembler/assembler.py
+++ Simple-Assembler/assembler.py
@@ -5,8 +5,10 @@ import compiler
 
 def main():
     # code = sys.stdin.readlines()
-    #code=open('/Users/aayushgakhar/Documents/GitHub/CO_M21_Assignment/automatedTesting/tests/assembly/errorGen/test1')
-    code = open('/CO/ass co/CO_M21_Assignment/automatedTesting/tests/assembly/errorGen/test1')
+    try:
+        code=open('/Users/aayushgakhar/Documents/GitHub/CO_M21_Assignment/automatedTesting/tests/assembly/errorGen/test1')
+    except:
+        code = open('/CO/ass co/CO_M21_Assignment/automatedTesting/tests/assembly/errorGen/test1')
     print([i for i in code])
     if(error_detection.check(code)):
         print('error')
