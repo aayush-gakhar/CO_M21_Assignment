8373f487b8880b58b5d27d4c4932974edc62766f
Aayush Gakhar <aayush20006@iiitd.ac.in>
2021-08-08 11:46:24 +0530

    3 tests passed(sim)😌

--- SimpleSimulator/simulator.py
+++ SimpleSimulator/simulator.py
@@ -3,15 +3,14 @@ import system
 
 
 def main():
-    # code = sys.stdin.readlines()
-    # print(code)
+    # code = list(map(str.strip,sys.stdin.readlines()))
     try:
         # '/Users/aayushgakhar/Documents/GitHub/CO_M21_Assignment/automatedTesting/tests/assembly/errorGen'
         #             '/test1'
         # path = '/Users/aayushgakhar/Documents/GitHub/CO_M21_Assignment/automatedTesting/tests/assembly/hardBin/test2'
 
-        path = '/Users/aayushgakhar/Documents/GitHub/CO_M21_Assignment/automatedTesting/tests/bin/simple/test1'
-        code = list(map(str.strip,open(path).readlines()))
+        # path = '/Users/aayushgakhar/Documents/GitHub/CO_M21_Assignment/automatedTesting/tests/bin/simple/test2'
+        # code = list(map(str.strip,open(path).readlines()))
     except:
         try:
             path = '/Users/aayushgakhar/Documents/GitHub/CO_M21_Assignment/automatedTesting/tests/bin/simple/test1'
--- SimpleSimulator/system.py
+++ SimpleSimulator/system.py
@@ -31,52 +31,77 @@ class RF:
         sys.stdout.write('\n')
 
     def get(self, reg):
-        pass
+        return self.REG[reg]
 
-    def set(self,reg,val):
-        pass
+    def set(self, reg, val):
+        if type(val)==int:
+            if 0<=val<2**16:
+                b=bin(val)[2:]
+                val='0'*(16-len(b))+b
+            else:
+                val='0000000000000000'
+                self.set_flag('V')
+        self.REG[reg] = val
+
+    def set_flag(self, s):
+        if s == 'V': # overflow
+            self.REG['111'] = '0000000000001000'
+        elif s == 'L': # less than
+            self.REG['111'] = '0000000000000100'
+        elif s == 'G': # greater than
+            self.REG['111'] = '0000000000000010'
+        elif s == 'E': # equal
+            self.REG['111'] = '0000000000000001'
+
+    def reset_flag(self):
+        self.REG['111'] = '0000000000000000'
 
 
 class PC:
     def __init__(self):
         self.pc = 0
 
+    def __index__(self):
+        return self.pc
+
     def dump(self):
-        sys.stdout.write(conv_to_bin(self.pc))
+        sys.stdout.write(conv_to_bin(self.pc)+' ')
 
     def get(self):
         return self.pc
 
-    def update(self,new_pc):
-        if new_pc==-1:
-            self.pc+=1
+    def update(self, new_pc):
+        if new_pc == -1:
+            self.pc += 1
         else:
-            self.pc=new_pc
+            self.pc = new_pc
+
 
 def execute(instruction, a):
     # return halted,new_pc(-1 if no jump)
     if instruction == '1001100000000000':
         return True, 0
     mem, pc, rf = a
-    opcode=instruction[:5]
-    if opcode in ['00000','00001','00110','01010','01011','01100']: # A
+    opcode = instruction[:5]
+    if opcode in ['00000', '00001', '00110', '01010', '01011', '01100']:  # A
+        rf.reset_flag()
         r1 = instruction[7:10]
         r2 = instruction[10:13]
         r3 = instruction[13:]
-        if opcode=='00000':
-            rf
-        elif opcode=='00001':
-            pass
-        elif opcode == '00110':
-            pass
-        elif opcode == '01010':
-            pass
-        elif opcode == '01011':
-            pass
-        elif opcode == '01100':
-            pass
+        if opcode == '00000':  # addition
+            rf.set(r1, int(rf.get(r2), 2) + int(rf.get(r2), 2))
+        elif opcode == '00001':  # substraction
+            rf.set(r1, int(rf.get(r2), 2) - int(rf.get(r2), 2))
+        elif opcode == '00110':  # multiply
+            rf.set(r1, int(rf.get(r2), 2) * int(rf.get(r2), 2))
+        elif opcode == '01010':  # XOR
+            rf.set(r1, int(rf.get(r2), 2) ^ int(rf.get(r2), 2))
+        elif opcode == '01011':  # or
+            rf.set(r1, int(rf.get(r2), 2) & int(rf.get(r2), 2))
+        elif opcode == '01100':  # and
+            rf.set(r1, int(rf.get(r2), 2) | int(rf.get(r2), 2))
 
-    elif opcode in ['00010','01000','01001']: # B
+    elif opcode in ['00010', '01000', '01001']:  # B
         if opcode == '00010':
             pass
         elif opcode == '01000':
@@ -84,7 +109,7 @@ def execute(instruction, a):
         elif opcode == '01001':
             pass
 
-    elif opcode in ['00011','00111','01101','01110']: # C
+    elif opcode in ['00011', '00111', '01101', '01110']:  # C
         if opcode == '00011':
             pass
         elif opcode == '00111':
@@ -94,13 +119,13 @@ def execute(instruction, a):
         elif opcode == '01110':
             pass
 
-    elif opcode in ['00100','00101']: # D
+    elif opcode in ['00100', '00101']:  # D
         if opcode == '00100':
             pass
         elif opcode == '00101':
             pass
 
-    else: # E
+    else:  # E
         if opcode == '01111':
             pass
         elif opcode == '10000':
@@ -110,9 +135,7 @@ def execute(instruction, a):
         elif opcode == '10010':
             pass
 
-
-class EE:
-    pass
+    return False, -1
 
 
 def conv_to_bin(n):
@@ -132,8 +155,16 @@ def run(code):
     a = [mem, pc, rf]
     while not halted:
         Instruction = mem.load(pc)
-        halted, new_pc = execute(Instruction,a)
+        halted, new_pc = execute(Instruction, a)
         pc.dump()
         rf.dump()
         pc.update(new_pc)
     mem.dump()
+
+
+reg_codes = {'R0': '000', 'R1': '001', 'R2': '010', 'R3': '011', 'R4': '100', 'R5': '101',
+             'R6': '110', 'FLAGS': '111'}
+
+# mem ==> 16bit
+# reg ==> 16bit
+# pc ==>8bit
