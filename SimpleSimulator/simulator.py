008b63a927f4f4f67710b320e06e168769dd51c9
Aayush Gakhar <aayush20006@iiitd.ac.in>
2021-08-13 23:08:28 +0530

    🥱

--- SimpleSimulator/simulator.py
+++ SimpleSimulator/simulator.py
@@ -4,11 +4,12 @@ import system
 
 def main():
     code = list(map(str.strip,sys.stdin.readlines()))
+
     # try:
     #     # path = '/Users/aayushgakhar/Desktop/test 2'
     #
     #     path = '/Users/aayushgakhar/Documents/GitHub/CO_M21_Assignment/automatedTesting/tests/bin/hard/test2'
-    #     code = list(map(str.strip,open(path).readlines()))
+    #     code = list(map(str.strip, open(path).readlines()))
     # except:
     #     try:
     #         path = '/Users/aayushgakhar/Documents/GitHub/CO_M21_Assignment/automatedTesting/tests/bin/simple/test1'
@@ -18,5 +19,6 @@ def main():
     #         code = list(map(str.strip, open(path).readlines()))
 
     system.run(code)
+
 
 main()
--- SimpleSimulator/system.py
+++ SimpleSimulator/system.py
@@ -7,20 +7,19 @@ class MEM:
         self.memory = code + ['0000000000000000'] * (256 - len(code))
 
     def dump(self):
-        # sys.stdout.write('\n')
         for i in self.memory:
             sys.stdout.write(i + '\n')
 
     def load(self, ind):
-        if type(ind)== str:
-            ind=conv_to_dec(ind)
+        if type(ind) == str:
+            ind = conv_to_dec(ind)
         return self.memory[ind]
 
     def store(self, ind, val):
         if type(val) == int:
-            val = conv_to_bin(val,16)
-        if type(ind)== str:
-            ind=conv_to_dec(ind)
+            val = conv_to_bin(val, 16)
+        if type(ind) == str:
+            ind = conv_to_dec(ind)
         self.memory[ind] = val
 
 
@@ -39,23 +38,23 @@ class RF:
         return self.REG[reg]
 
     def set(self, reg, val):
-        if type(val)==int:
-            if 0<=val<2**16:
-                b=bin(val)[2:]
-                val='0'*(16-len(b))+b
+        if type(val) == int:
+            if 0 <= val < 2 ** 16:
+                b = bin(val)[2:]
+                val = '0' * (16 - len(b)) + b
             else:
-                val='0000000000000000'
+                val = '0000000000000000'
                 self.set_flag('V')
         self.REG[reg] = val
 
     def set_flag(self, s):
-        if s == 'V': # overflow
+        if s == 'V':  # overflow
             self.REG['111'] = '0000000000001000'
-        elif s == 'L': # less than
+        elif s == 'L':  # less than
             self.REG['111'] = '0000000000000100'
-        elif s == 'G': # greater than
+        elif s == 'G':  # greater than
             self.REG['111'] = '0000000000000010'
-        elif s == 'E': # equal
+        elif s == 'E':  # equal
             self.REG['111'] = '0000000000000001'
 
     def reset_flag(self):
@@ -70,7 +69,7 @@ class PC:
         return self.pc
 
     def dump(self):
-        sys.stdout.write(conv_to_bin(self.pc)+' ')
+        sys.stdout.write(conv_to_bin(self.pc) + ' ')
 
     def get(self):
         return self.pc
@@ -109,33 +108,33 @@ def execute(instruction, a):
 
     elif opcode in ['00010', '01000', '01001']:  # B
         rf.reset_flag()
-        r1=instruction[5:8]
-        imm=instruction[8:]
-        if opcode == '00010': # move
-            rf.set(r1, '00000000'+imm)
-        elif opcode == '01000': # rs
-            rf.set(r1, int(rf.get(r1), 2)>>imm)
-        elif opcode == '01001': # ls
-            rf.set(r1, (int(rf.get(r1), 2)<<imm)%2**16)
+        r1 = instruction[5:8]
+        imm = instruction[8:]
+        if opcode == '00010':  # move
+            rf.set(r1, '00000000' + imm)
+        elif opcode == '01000':  # rs
+            rf.set(r1, int(rf.get(r1), 2) >> imm)
+        elif opcode == '01001':  # ls
+            rf.set(r1, (int(rf.get(r1), 2) << imm) % 2 ** 16)
 
     elif opcode in ['00011', '00111', '01101', '01110']:  # C
-        if opcode!='00011':
+        if opcode != '00011':
             rf.reset_flag()
         r1 = instruction[10:13]
         r2 = instruction[13:]
-        if opcode == '00011': # move
-            rf.set(r1,rf.get(r2))
+        if opcode == '00011':  # move
+            rf.set(r1, rf.get(r2))
             rf.reset_flag()
-        elif opcode == '00111': # divide
+        elif opcode == '00111':  # divide
             rf.set('000', int(rf.get(r1), 2) / int(rf.get(r2), 2))
             rf.set('001', int(rf.get(r1), 2) % int(rf.get(r2), 2))
-        elif opcode == '01101': # invert
+        elif opcode == '01101':  # invert
             rf.set(r1, ''.join('1' if i == '0' else '0' for i in rf.get(r2)))
-        elif opcode == '01110': # compare
-            a,b = int(rf.get(r1), 2) , int(rf.get(r2), 2)
-            if a<b:
+        elif opcode == '01110':  # compare
+            a, b = int(rf.get(r1), 2), int(rf.get(r2), 2)
+            if a < b:
                 rf.set_flag('L')
-            elif a>b:
+            elif a > b:
                 rf.set_flag('G')
             else:
                 rf.set_flag('E')
@@ -144,18 +143,18 @@ def execute(instruction, a):
         rf.reset_flag()
         r1 = instruction[5:8]
         mem_addr = instruction[8:]
-        if opcode == '00100': # load
-            rf.set(r1,mem.load(mem_addr))
-        elif opcode == '00101': # store
-            mem.store(mem_addr,rf.get(r1))
+        if opcode == '00100':  # load
+            rf.set(r1, mem.load(mem_addr))
+        elif opcode == '00101':  # store
+            mem.store(mem_addr, rf.get(r1))
 
     else:  # E
         mem_addr = instruction[8:]
-        flag=rf.get('111')
+        flag = rf.get('111')
         if opcode == '01111':
             return False, conv_to_dec(mem_addr)
         elif opcode == '10000':
-            if flag[-3]=='1':
+            if flag[-3] == '1':
                 return False, conv_to_dec(mem_addr)
         elif opcode == '10001':
             if flag[-2] == '1':
@@ -168,7 +167,7 @@ def execute(instruction, a):
     return False, -1
 
 
-def conv_to_bin(n,l=8):
+def conv_to_bin(n, l=8):
     b = bin(n)[2:]
     return (l - len(b)) * '0' + b
 
@@ -177,11 +176,11 @@ def conv_to_dec(b):
     return int(b, 2)
 
 
-def run(code,scatter=False):
+def run(code, scatter=False):
     mem = MEM(code)
     pc = PC()
     rf = RF()
-    addr=[]
+    addr = []
     halted = False
     a = [mem, pc, rf]
     while not halted:
@@ -193,15 +192,9 @@ def run(code,scatter=False):
         pc.update(new_pc)
     mem.dump()
     if scatter:
-        plt.scatter([i+1 for i in range(len(addr))],addr,s=[5 for i in addr],cmap='viridis')
-        # print(addr)
-        # plt.xticks([i for i in range(0,len(addr),1+len(addr)//10)])
+        plt.scatter([i + 1 for i in range(len(addr))], addr, s=5, cmap='viridis')
         plt.show()
 
-
-
-reg_codes = {'R0': '000', 'R1': '001', 'R2': '010', 'R3': '011', 'R4': '100', 'R5': '101',
-             'R6': '110', 'FLAGS': '111'}
 
 # mem ==> 16bit
 # reg ==> 16bit
