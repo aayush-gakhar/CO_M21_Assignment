d41473e1131b854a7a5d102fdaa52e1509233cb2
Aayush Gakhar <aayush20006@iiitd.ac.in>
2021-08-15 11:00:03 +0530

    final asm

--- Simple-Assembler/assembler.py
+++ Simple-Assembler/assembler.py
@@ -6,20 +6,20 @@ import compiler
 def main():
     code = [i.split() for i in sys.stdin.readlines()]
 
-    try:
-        # '/Users/aayushgakhar/Documents/GitHub/CO_M21_Assignment/automatedTesting/tests/assembly/errorGen'
-        #             '/test1'
-        # path='/Users/aayushgakhar/Documents/GitHub/CO_M21_Assignment/automatedTesting/tests/assembly/hardBin/test2'
-
-        path = '/Users/aayushgakhar/Desktop/test'
-        code = [i.split() for i in open(path).readlines()]
-    except:
-        try:
-            code = [i.split() for i in
-                    open('/CO/ass co/CO_M21_Assignment/automatedTesting/tests/assembly/errorGen/test1').readlines()]
-        except:
-             path = 'C:/Users/hp/Documents/GitHub/assignment 1/automatedTesting/tests/assembly/errorGen/test3'
-             code = [i.split() for i in open(path).readlines()]
+    # try:
+    #     # '/Users/aayushgakhar/Documents/GitHub/CO_M21_Assignment/automatedTesting/tests/assembly/errorGen'
+    #     #             '/test1'
+    #     # path='/Users/aayushgakhar/Documents/GitHub/CO_M21_Assignment/automatedTesting/tests/assembly/hardBin/test2'
+    #
+    #     path = '/Users/aayushgakhar/Desktop/test'
+    #     code = [i.split() for i in open(path).readlines()]
+    # except:
+    #     try:
+    #         code = [i.split() for i in
+    #                 open('/CO/ass co/CO_M21_Assignment/automatedTesting/tests/assembly/errorGen/test1').readlines()]
+    #     except:
+    #          path = 'C:/Users/hp/Documents/GitHub/assignment 1/automatedTesting/tests/assembly/errorGen/test3'
+    #          code = [i.split() for i in open(path).readlines()]
 
     has_error, var, labels, instruction_number = error_detection.check(code)
     if not has_error:
